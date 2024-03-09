e32af80ef9dfe6baa737852a94ea36ad8be1c1b9
Liam5095 <gamerliam12@outlook.com>
2024-03-09 16:37:46 +0100

    Item And CreativeTab

---
+++ src/main/java/net/wickedbog/tb_reloaded/item/ModCreativeModTabs.java
@@ -0,0 +1,28 @@
+package net.wickedbog.tb_reloaded.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.wickedbog.tb_reloaded.TBReloaded;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TBReloaded.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.EXAMPLE_ITEM.get()))
+                    .title(Component.translatable("creativetab.example_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.EXAMPLE_ITEM.get());
+                    })
+                    .withSearchBar()
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/wickedbog/tb_reloaded/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.wickedbog.tb_reloaded.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.wickedbog.tb_reloaded.TBReloaded;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TBReloaded.MOD_ID);
+
+    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
